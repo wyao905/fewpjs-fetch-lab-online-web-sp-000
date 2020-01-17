dd2f48d404ea550bdb77a7bfd7d706f652bafc7c
Warren Yao <warrenyca905@gmail.com>
2020-01-17 03:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function fetchBooks {
-  fetch("https://anapioficeandfire.com/api/books")
-    .then(resp => resp.json())
-    .then(function(json){
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+          .then(resp => resp.json())
+          .then(json => console.log(json));
 }
 
 function renderBooks(json) {
